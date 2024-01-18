39bd01061f98549ebf33abe1261136002a2d12f6
Nikita Biryukov <chppppr@yandex.ru>
2024-01-18 21:24:44 +0700

    Refactor the xor demo

--- demos/xor/main.cpp
+++ demos/xor/main.cpp
@@ -1,57 +1,49 @@
 #include <iostream>
 
-#include <nn/nn.hpp>
+#include <tobuso/nn.hpp>
 
 int main()
 {
     srand(time(0));
-    double xor_in[] = {0, 0, 0, 1, 1, 0, 1, 1};
-    double xor_out[] = {0, 1, 1, 0};
+    tbs::Matrices in = {
+            {1, 2, {0, 0}},
+            {1, 2, {0, 1}},
+            {1, 2, {1, 0}},
+            {1, 2, {1, 1}},
+    };
+    tbs::Matrices out = {
+            {1, 1, {0}},
+            {1, 1, {1}},
+            {1, 1, {1}},
+            {1, 1, {0}},
+    };
 
-    std::vector<Matrix> in;
-    for (int i = 0; i < 4; ++i) {
-        Matrix mat;
-        mat.m = &xor_in[i * 2];
-        mat.cols = 2;
-        mat.rows = 1;
-
-        in.push_back(mat);
-    }
-
-    std::vector<Matrix> out;
-    for (int i = 0; i < 4; ++i) {
-        Matrix mat;
-        mat.m = &xor_out[i];
-        mat.cols = 1;
-        mat.rows = 1;
-
-        out.push_back(mat);
-    }
-
-    size_t layers[] = {2, 2, 1};
-    NN nn = NN(layers, ARRAY_LEN(layers));
+    size_t input_size = 2;
+    tbs::Layers layers = {{2, tbs::SIGMOID}, {1, tbs::SIGMOID}};
+    tbs::NN nn(input_size, layers);
     nn.rand(-1, 1);
 
-    NN gradient(nn.get_layers(), nn.get_layers_count());
-
+    tbs::NN gradient(input_size, layers);
     double cost = nn.cost(in, out);
+    double rate = 1;
+
     printf("%d: %lf\n", 0, cost);
-    for (size_t i = 1; i <= 10000; i++) {
+    for (size_t i = 1; i <= 10 * 1000; i++) {
         cost = nn.cost(in, out);
         printf("%ld: cost = %lf\n", i, cost);
-        nn.backprop(gradient, in, out, 1);
+        nn.backprop(gradient, in, out, rate);
     }
 
-    std::cout << "\nTESTING XOR:\n";
+    std::cout << "\nTESTING XOR:\n\n";
+    cost = nn.cost(in, out);
+    printf("Cost: %lf\n", cost);
     int k = 0;
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
             nn.forward(in[k++]);
 
-            std::cout << "in: " << i << ' ' << j
-                      << " out: " << nn.get_out().m[0] << '\n';
+            std::cout << "in: " << i << ' ' << j << " out: ";
+            nn.output()->print();
         }
     }
-
-    return 0;
 }
